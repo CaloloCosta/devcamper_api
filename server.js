c09a01c8f69fb182a992d55e13e00e21beaeb5f3
calolocosta <carlosdaviddacosta@gmail.com>
2021-07-15 18:33:43 +0200

    module 3 complete

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
-const express = require('express')
-const dotenv = require('dotenv')
-
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan")
+// Route files
+const bootcamps = require("./routes/bootcamps");
+// logger
+const logger = require("./middleware/logger")
 // load env vars
-dotenv.config({path: './config/config.env'})
+dotenv.config({ path: "./config/config.env" });
 
-const app = express()
+const app = express();
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on ${PORT}`))
+// dev logging middlewar
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'))
+}
+// Mount routers
+app.use("/api/v1/bootcamps", bootcamps);
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on ${PORT}`)
+);
