07eebe953f5d4446e067d1e68da160f212afaa79
calolocosta <carlosdaviddacosta@gmail.com>
2021-08-05 18:44:17 +0200

    basic CRUD

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan")
+const colors = require("colors")
+const connectDB = require('./config/db')
 // Route files
 const bootcamps = require("./routes/bootcamps");
 // logger
@@ -8,18 +10,31 @@ const logger = require("./middleware/logger")
 // load env vars
 dotenv.config({ path: "./config/config.env" });
 
+// connect to db
+connectDB()
+
 const app = express();
+
+// Body parser
+app.use(express.json())
 
 // dev logging middlewar
 if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'))
-}
+} 
 // Mount routers
 app.use("/api/v1/bootcamps", bootcamps);
 
 
 const PORT = process.env.PORT || 5000;
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on ${PORT}`)
+  console.log(`Server running in ${process.env.NODE_ENV} mode on ${PORT}`.yellow.bold)
 );
+
+
+// handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red)
+  server.close(() => process.exit(1))
+})
